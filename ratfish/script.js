463f04e61192bd2ba227ef3ed42fe0dc1d46e605
FinlayWales <finlaywales@gmail.com>
2024-09-19 23:17:53 +0100

    end to end socket test

--- ratfish/script.js
+++ ratfish/script.js
@@ -1,5 +1,6 @@
 let messageInputBox = document.getElementById("message-input-box");
 let sendButton = document.getElementById("send-button");
+let messageOutput = document.getElementById("message-output");
 
 // Textbox Resizing
 sendButton.style.height = messageInputBox.offsetHeight + "px";
@@ -9,11 +10,17 @@ messageInputBox.addEventListener("input", function(e) {
     e.target.style.height = e.target.scrollHeight - 30 + "px";
 });
 
-// Socket
+// Sockets
 const socket = io();
 sendButton.addEventListener("click", function(e) {
     if (messageInputBox.value) {
         socket.emit('message', messageInputBox.value);
         messageInputBox.value = '';
     }
+});
+
+socket.on('message', (msg) => {
+    const item = document.createElement('div');
+    item.textContent = msg;
+    messageOutput.appendChild(item);
 });
