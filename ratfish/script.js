46a4e033a7290728a3519b5b3ec7167959da6b0d
FinlayWales <finlaywales@gmail.com>
2024-09-19 17:39:59 +0100

    ratfish ui stuff

---
+++ ratfish/script.js
@@ -0,0 +1,10 @@
+// Textbox Resizing
+let messageInputBox = document.getElementById("message-input-box");
+let sendButton = document.getElementById("send-button");
+
+sendButton.style.height = messageInputBox.offsetHeight + "px";
+
+messageInputBox.addEventListener("input", function(e) {
+    e.target.style.height = "auto";
+    e.target.style.height = e.target.scrollHeight - 30 + "px";
+});
